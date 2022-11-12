be496ac8d4093d668863adae06489afbf87a3be5
noceive <nocivescience@gmail.com>
2022-11-12 16:17:20 -0300

    empiezan los formularios

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .forms import TaskForm
+from .models import Task
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def home(request):
     return render(request,'home.html',)
@@ -27,8 +30,15 @@ def signup(request):
             'form':UserCreationForm,
             'error':'Passwords dont match'
         })
+@login_required
 def tasks(request):
-    return render(request, 'tasks.html')
+    tasks=Task.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, 'tasks.html',{'tasks':tasks})
+@login_required
+def tasks_completed(request):
+    tasks=Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, 'tasks.html',{'tasks':tasks})
+@login_required
 def signout(request):
     logout(request)
     return redirect('home')
@@ -43,6 +53,7 @@ def signin(request):
         else:
             login(request,user)
             return redirect('tasks')
+@login_required
 def create_task(request):
     if request.method=='GET':
         return render(request,'create_task.html',{'form': TaskForm})
@@ -55,3 +66,29 @@ def create_task(request):
             return redirect('tasks')
         except ValueError:
             return render(request,'create_task.html',{'form': TaskForm, 'error': 'algo muy grave ha sucedido'})
+@login_required
+def task_detail(request, task_id):
+    if request.method=='GET':
+        task=get_object_or_404(Task,pk=task_id, user=request.user)
+        form=TaskForm(instance=task)
+        return render(request,'task_detail.html',{'task':task, 'form': form})
+    else:
+        try:
+            task=get_object_or_404(Task,pk=task_id, user=request.user)
+            form=TaskForm(request.POST,instance=task)
+            form.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request,'task_detail.html',{'task':task, 'form': form, 'error': 'Something was wrong'})
+@login_required
+def complete_task(request,task_id):
+    task=get_object_or_404(Task, pk=task_id, user=request.user)
+    task.datecompleted=timezone.now()
+    task.save()
+    return redirect('tasks')
+@login_required
+def delete_task(request,task_id):
+    task=get_object_or_404(Task, pk=task_id, user=request.user)
+    task.datecompleted=timezone.now()
+    task.delete()
+    return redirect('tasks')
